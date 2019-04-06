21f823fa588905b0b85dbbd23e6782c808dea697
NogiZone <49055915+NogiZone@users.noreply.github.com>
2019-04-06 18:59:56 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quiz</title>
+</head>
+<body>
+    <Form method="post" action="form.php">
+        <label for="P1">¿Cuál es la capital del Perú?</label><br><br> 
+        <input type="text" name="R1" class="form-control"><br> <br> 
+        <input type="submit" name="submit" value="Enviar"><br> <br> 
+    </Form method= "post">
+</body>
+</html>
